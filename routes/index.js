ade9c6382e8fc5dae5d6e8dcc7d6a0cb76376066
Shams Misbah <mailshamsmisbah@gmail.com>
2023-11-18 01:23:50 +0530

    adding a new route and controller file for showing profile

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const router = express.Router();
+
 const homeController = require('../controllers/home_controller');
 
-const router = express.Router();
-router.get('/',homeController.home)
+
+router.get('/',homeController.home);
+router.use('/users',require('./users'));
 console.log('router loaded');
 module.exports=router;
---
+++ routes/users.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+const userscontroller = require('../controllers/users_controller');
+router.get('/profile',userscontroller.profile);
+module.exports=router;
