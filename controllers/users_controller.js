a80adaf10c0bac4002027f723363adda0c0d72f4
Shams Misbah <mailshamsmisbah@gmail.com>
2023-11-26 18:22:54 +0530

    adding user profile page showing details of user after logging in

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,21 @@
 const User = require('../models/user');
+
 module.exports.profile = function(req,res){
-    res.end('<h1>User Profile </h1>');
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id,function(err,user){
+            if(user){
+                return res.render('user_profile',{
+                    title : " User Profile",
+                    user : user
+                })
+            }else{
+            return res.redirect('/users/sign-in');
+            }
+        });
+    }
+    else{
+        return res.redirect('/users/sign-in');
+    }
 }
 //signin page rendering 
 module.exports.signUp = function(req,res){
@@ -47,5 +62,24 @@ module.exports.create = function(req,res){
 
 //logging in a user
 module.exports.createSession = function(req,res){
-    // todo later
+    User.findOne({email : req.body.email},function(err,user){
+        if(err){
+            console.log('error in finding user in signing up');
+            return}
+            
+           if(user){
+               //handle passwords that dont match
+               if(user.password != req.body.password){
+                return res.redirect('back');
+               }
+               //handle session creation
+               res.cookie('user_id',user.id);
+               return res.redirect('/users/profile');
+           }
+           
+           else{
+             //handle user/account not found in db
+             return res.redirect('back');
+           }
+});
 }
